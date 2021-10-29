817f7fc95a88dd2badd2554c8bdcfd520347fa33
Ilton Barbosa <jiltonbarbosa@gmail.com>
2021-10-29 18:44:25 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import { StatusBar } from 'react-native';
+import { Home } from './src/pages/Home';
+import SplashScreen from 'react-native-splash-screen';
+
+export default function App(){
+
+  useEffect(() => {
+    SplashScreen.hide();
+  },[]);
+
+  return (
+    <>
+    <StatusBar barStyle="dark-content"/>
+    <Home />
+    </>
+  )}
+
---
+++ src/pages/Home.tsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from 'react';
+import { View, 
+        Text, 
+        StyleSheet, 
+        TextInput, 
+        Platform,
+        FlatList,
+     } from 'react-native';
+import { Button } from '../components/Button';
+import { SkillCard } from '../components/SkillCard';
+
+interface SkillData {
+  id: string;
+  name: string;
+}
+
+export function Home(){
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
+  const [greeting, setGreeting] = useState('');
+
+  //sempre que uma função for disparada 
+  //a partir de uma interação com usuário,
+  //é boa prática iniciar com o termo "handle"
+  function handleAddNewSkill(){
+    const data = {
+      id:String(new Date().getTime()),
+      name: newSkill
+    }
+    setMySkills(oldState => [...oldState, data]);
+  }
+
+  function handleRemoveSkill(id: string){
+      setMySkills(oldState => oldState.filter(
+          skill => skill.id !== id
+      ))
+  }
+
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+  
+    if (currentHour < 12) {
+      setGreeting('Good morning');
+    }else 
+      if (currentHour >= 12 && currentHour < 18){
+        setGreeting('Good afternoon');
+      }else{
+        setGreeting('Good night');
+      }
+  },[])
+
+  return (
+      <View style={styles.container}>
+        
+        <Text style={styles.title} testID="welcome">
+            Welcome Ilton Barbosa
+        </Text>
+
+        <Text style={styles.greetings}>
+         {greeting}
+        </Text>
+
+        <TextInput 
+            testID="input-new"
+            style={styles.input}
+            placeholder="New skill"
+            placeholderTextColor="#555"
+            onChangeText={setNewSkill}
+        />
+
+        <Button 
+          testID="button-add"
+          onPress={handleAddNewSkill}
+          title="Add"
+        />
+
+        <Text style={[styles.title, { marginVertical:50}]}>
+          My Skills
+        </Text>
+
+        {mySkills &&
+          <FlatList
+              testID="flat-list-skills"
+              data={mySkills}
+              keyExtractor={item => item.id}
+              keyboardShouldPersistTaps="never"
+              renderItem={( { item }) => (
+                <SkillCard 
+                    skill={item.name}
+                    onPress={() => handleRemoveSkill(item.id)}
+                    />
+              )}
+          />
+        }
+
+      </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    backgroundColor: '#171015',
+    paddingHorizontal: 30,
+    paddingVertical: 70,
+  },
+  title:{
+    color: '#FFF',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: '#1F1E25',
+    color: '#FFF',
+    fontSize: 18,
+    padding: Platform.OS == 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7
+  },
+  greetings: {
+    color: '#FFF'
+  }
+})
+
